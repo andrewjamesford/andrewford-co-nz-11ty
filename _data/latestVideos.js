2477a95f0baa5a7e25534c9b510a0d5bade74069
Andrew Ford <andrew@andrewford.co.nz>
2023-09-25 14:21:59 +1300

    Displaying latest videos titles on home page

---
+++ _data/latestVideos.js
@@ -0,0 +1,23 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+
+module.exports = async function() {
+    try {
+        const baseUrl = process.env.SITE_URL || "http://localhost:8888";
+        let url = baseUrl + "/.netlify/functions/latestUploads";
+      
+        let json = await EleventyFetch(url, {
+          duration: "1d", // save for 1 day
+          type: "json"    // we’ll parse JSON for you
+        });
+
+        return {
+            videos: json
+        };
+
+    } catch (e) {
+        console.log(e);
+        return {
+            videos: []
+        };
+    }
+};
