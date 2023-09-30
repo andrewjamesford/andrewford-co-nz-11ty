181c387b034e4ad668023e60758714e0631c31fc
Andrew Ford <andrew@andrewford.co.nz>
2023-09-30 22:07:58 +1300

    Filter tags displayed

--- eleventy.config.js
+++ eleventy.config.js
@@ -77,7 +77,7 @@ module.exports = function (eleventyConfig) {
 
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
 		return (tags || []).filter(
-			(tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+			(tag) => ["all", "nav", "post", "posts", "[articles]", "[archive]"].indexOf(tag) === -1
 		);
 	});
 
