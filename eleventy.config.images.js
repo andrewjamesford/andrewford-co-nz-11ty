959f68be17f78e95087e5994ede93dd8803f842d
Andrew Ford <andrew@andrewford.co.nz>
2023-10-01 19:36:11 +1300

    Added external image shortcode and implemented on home

--- eleventy.config.images.js
+++ eleventy.config.images.js
@@ -35,6 +35,32 @@ module.exports = (eleventyConfig) => {
 		}
 	);
 
+	// Eleventy Image shortcode
+	// https://www.11ty.dev/docs/plugins/image/
+	eleventyConfig.addAsyncShortcode(
+		"externalImage",
+		async function externalImageShortcode(src, alt, widths, sizes, cssClass) {
+			// Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
+			// Warning: Avif can be resource-intensive so take care!
+			let file = src;
+			let metadata = await eleventyImage(file, {
+				widths: widths || ["auto"],
+				formats,
+				outputDir: path.join(eleventyConfig.dir.output, "img"), // Advanced usage note: `eleventyConfig.dir` works here because we’re using addPlugin.
+			});
+
+			// TODO loading=eager and fetchpriority=high
+			let imageAttributes = {
+				alt,
+				sizes,
+				loading: "lazy",
+				decoding: "async",
+				class: cssClass,
+			};
+			return eleventyImage.generateHTML(metadata, imageAttributes);
+		}
+	);
+
 	eleventyConfig.addAsyncShortcode(
 		"figure",
 		async function imageShortcode(src, alt, widths, sizes) {
