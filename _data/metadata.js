6a25de381836708a442d8487f89c07f40915ce0b
Andrew Ford <andrew@andrewford.co.nz>
2025-05-27 16:53:20 +1200

    Add apiUrl to metadata.js for improved configuration management

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   title: "Andrew Ford",
   fullUrl: process.env.SITE_URL,
+  apiUrl: process.env.API_URL,
   language: "en",
   description:
     "Andrew Ford is the host of Code with Andrew Ford, a YouTube channel dedicated to teaching you how to code, a full-stack web developer, mentor and an educator.",
